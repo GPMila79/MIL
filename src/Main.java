30c1c580ee65ca05e04a572af41e517cbdb483c6
Mila <lu.pshenichnikova@gmail.com>
2022-12-15 15:14:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 60000;
+        int onemili = 20;
+        int bonus = ticket / onemili;
+
+        System.out.println((bonus) + " Количество бонусных миль");
+    }
+}
